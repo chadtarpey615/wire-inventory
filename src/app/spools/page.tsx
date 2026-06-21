10fa0bf94e204697e4f454e18d320df66f91bbb0
chadtarpey615 <chadtarpey@gmail.com>
2026-06-21 10:03:44 -0500

    added to .env

--- src/app/spools/page.tsx
+++ src/app/spools/page.tsx
@@ -1,6 +1,6 @@
 import SpoolForm from "@/components/inventory/spool-form";
 import SpoolTable from "@/components/inventory/spool-table";
-
+console.log("site_url", process.env.SITE_URL);
 export default function SpoolsPage() {
   return (
     <div className="space-y-8">
